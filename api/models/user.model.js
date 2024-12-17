dba7f3a86d95d0156afe5a9dcfc7e09f3389af86
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-12-17 22:31:11 +0530

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:"String",
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:"String",
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:"String",
+        required:true,
+    },
+    firstname:{
+        type:"String",
+    },
+    lastname:{
+        type:"String",
+    },
+    about:{
+        type:"String",
+    },
+
+},{timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
