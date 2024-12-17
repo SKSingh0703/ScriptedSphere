b52d214aa2d5434ca05b95ca4d097c860fff6e4e
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-12-17 16:45:32 +0530

    Created Header Section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");
+import { content , plugin } from "flowbite-react/tailwind";
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(),
+  ],
 }
 
